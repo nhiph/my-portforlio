f3261dd554db759c7c21cd9e8ef5f82562c13ed5
nhiph <phunghuynhnhi797@gmail.com>
2024-12-05 23:11:30 +0700

    update mobile

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -7,9 +7,13 @@ const ProjectLayout = ({ name, description, demoLink, techs }) => {
       <h1 className="font-bold mb-2">{name}</h1>
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-center space-x-2 mr-2">
-          <p className="text-muted hidden sm:inline-block">{description}</p>
+          <p className="text-muted">{description}</p>
         </div>
-        <Link href={demoLink} target="_blank">
+        <Link
+          className="hidden sm:inline-block"
+          href={demoLink}
+          target="_blank"
+        >
           <Eye
             style={{ width: "20px" }}
             className="w-full h-auto"
@@ -17,11 +21,11 @@ const ProjectLayout = ({ name, description, demoLink, techs }) => {
           />
         </Link>
       </div>
-      <div className="w-full flex items-center justify-center mt-3">
+      <div className="w-full flex flex-wrap items-center justify-center mt-3">
         {techs.map((tech, index) => {
           return (
             <span
-              className="rounded-md shadow-lg bg-background border border-accent/30 border-solid
+              className="mb-2 rounded-md shadow-lg bg-background border border-accent/30 border-solid
       hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor-pointer capitalize mr-3 px-3 py-1"
               key={index}
             >
